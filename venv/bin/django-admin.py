34ad106c748c5e01962178fa88c49050ab6be118
Chidinma Umenwofor-Nweze <c.umenwofornweze@gmail.com>
2020-02-06 20:40:47 -0500

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chidinmaumenwofor-nweze/Desktop/side-projects/happynews/venv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
